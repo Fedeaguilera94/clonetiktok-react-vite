d6324f2d0868ccdb8e15821d902f78f09e612879
Fedeaguilera94 <aguilerafederico94@gmail.com>
2022-01-18 15:55:47 -0300

    fix heart add marquee

--- src/components/FeedVideos/index.jsx
+++ src/components/FeedVideos/index.jsx
@@ -9,9 +9,10 @@ const VIDEOS = [
     shares: 234,
     commets: 33,
     songTitle: "sonidoOriginal valentinvalverde12",
-    albumCover: "",
+    albumCover:
+      "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/b438a3b2c487261328517e5f98b6db32~c5_720x720.jpeg?x-expires=1642604400&x-signature=WK1Iji3uylxdseMqEQN%2BSAtkXUs%3D",
 
-    src: "https://v16-webapp.tiktok.com/ff7baa9473f56c31e5469e5256fa22bd/61de9575/video/tos/useast2a/tos-useast2a-pve-0068/a646ee2986894fc8b07647092598c0c0/?a=1988&br=1808&bt=904&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FIrkag3-I&l=202201120246290101921611680475FCFE&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anRvNmk6ZjRrOjMzNzczM0ApaWg8NWdlNzs4Nzk0NGg3aWdtczVrcjRvXy9gLS1kMTZzczVhYS81YTAyYV5fYTQxLjY6Yw%3D%3D&vl=&vr=",
+    src: "https://v16-webapp.tiktok.com/ebdef1fa30147fb5d8087867c418bf86/61e75997/video/tos/useast2a/tos-useast2a-ve-0068c003/7b0ddea709154e50a951685d81ced97e/?a=1988&br=2750&bt=1375&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3W_nz7ThP33ylXq&l=20220118182053010223016097144496C1&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajpscTg6ZnRzOTMzNzczM0ApaGhpZDk2NGQ4NzQ0M2k1O2dnNm5icjRfbjFgLS1kMTZzczBeMzVgXjRiLl9eY2E2XjE6Yw%3D%3D&vl=&vr=",
   },
   {
     id: 2,
@@ -21,8 +22,9 @@ const VIDEOS = [
     shares: 234,
     commets: 33,
     songTitle: "sonidoOriginal valentinvalverde12",
-    albumCover: "",
-    src: "https://v16-webapp.tiktok.com/83a73f23682a29be6c54468379081b23/61de9575/video/tos/useast2a/tos-useast2a-ve-0068c003/bf353afbf78b4f268edbd89f30121cc0/?a=1988&br=4426&bt=2213&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FIrkag3-I&l=202201120246290101921611680475FCFE&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3l4cGc6ZmozOjMzNzczM0ApPDVkZDgzPDs6N2Q5ZjU2PGcxMmJjcjRvamFgLS1kMTZzczMxMTUtYGA1X181NjEuMjI6Yw%3D%3D&vl=&vr=",
+    albumCover:
+      "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/b438a3b2c487261328517e5f98b6db32~c5_720x720.jpeg?x-expires=1642604400&x-signature=WK1Iji3uylxdseMqEQN%2BSAtkXUs%3D",
+    src: "https://v16-webapp.tiktok.com/a1e0fd1f0ed7ed306254525e1b0f407a/61e75980/video/tos/useast2a/tos-useast2a-pve-0068/54ae607338ed4b96881b2f5e15d36d5c/?a=1988&br=6220&bt=3110&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3W_nz7ThP33ylXq&l=20220118182053010223016097144496C1&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajhycDs6ZnA6OTMzNzczM0ApZDU5NDw5OmUzN2VpNDNlNWcyczVhcjRvaGVgLS1kMTZzc2E0MTVfYmBjNjQzYWJhMzA6Yw%3D%3D&vl=&vr=",
   },
 ];
 
--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,18 +1,37 @@
+import { Comment } from "../../Icons/Comment";
 import { Heart } from "../../Icons/Heart";
+import { Share } from "../../Icons/Share";
 import styles from "./styles.module.css";
 
-export default function VideoPlayerActions() {
+export default function VideoPlayerActions({
+  likes = 12,
+  commets = 22,
+  shares = 33,
+  hearted = false,
+}) {
+  const handleLike = () => {
+    alert("like");
+  };
+  const handleComment = () => {
+    alert("comment");
+  };
+  const handleShare = () => {
+    alert("share");
+  };
   return (
     <aside className={styles.actions}>
-      <div className={styles.action}>
+      <button onClick={handleLike} className={styles.action}>
         <Heart />
-      </div>
-      <div className={styles.action}>
-        <Heart />
-      </div>
-      <div className={styles.action}>
-        <Heart />
-      </div>
+        <span title="like">{likes}</span>
+      </button>
+      <button onClick={handleComment} className={styles.action}>
+        <Comment />
+        <span title="commets">{commets}</span>
+      </button>
+      <button onClick={handleShare} className={styles.action}>
+        <Share />
+        <span title="shares">{shares}</span>
+      </button>
     </aside>
   );
 }
